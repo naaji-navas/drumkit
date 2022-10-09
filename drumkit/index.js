1a99dc96dc649e1d6476f45674f9282ed98500a8
Najid Navas <104789565+naaji-navas@users.noreply.github.com>
2022-10-09 20:27:40 +0530

    added files via upload

---
+++ drumkit/index.js
@@ -0,0 +1,54 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keypress", (e) => {
+  makeSound(e.key);
+  buttonAnimation(e.key);
+});
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      var audio = new Audio("./sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("./sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("./sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("./sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("./sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("./sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("./sounds/tom-4.mp3");
+      audio.play();
+      break;
+    default:
+      console.log(buttonInnerHTML);
+  }
+}
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(() =>{
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
